98786e7b5696fa9e9657752e8eb3f50a9a1985fe
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-12-28 17:52:58 -0600

    Initialized CollisionDetect.java

---
+++ game_engine/backend/events/CollisionDetect.java
@@ -0,0 +1,17 @@
+package game_engine.backend.events;
+
+public class CollisionDetect extends Event{
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

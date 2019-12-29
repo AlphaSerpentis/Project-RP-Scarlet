09ceafa86b2590aff627abc4a30d02794edc90e7
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-12-28 18:43:51 -0600

    Initialize GraphicsEventHandler.java

---
+++ game_engine/backend/events/GraphicsEventHandler.java
@@ -0,0 +1,17 @@
+package game_engine.backend.events;
+
+public class GraphicsEventHandler extends Event {
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

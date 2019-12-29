fa95b3e5c991875c05d5fa714194fabae020f23c
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-12-28 18:58:26 -0600

    Added class Graphics w/ subclasses

---
+++ game_engine/backend/graphics/Graphics.java
@@ -0,0 +1,23 @@
+package game_engine.backend.graphics;
+
+public class Graphics {
+
+	/**
+	 * UserInterface class contains all the GUI functions
+	 *
+	 */
+	protected class UserInterface {
+		
+	}
+	
+	/**
+	 * Painter class contains all the graphical functions, excluding GUI functions, in which they draw stuff onto the screen.
+	 *
+	 */
+	protected class Painter {
+		
+	}
+	
+	
+	
+}

aaeedfbb5a3366d43d35964b7c84b7f9826ac45d
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2019-12-28 19:34:16 -0600

    Begin Graphics development

--- game/init/Launcher.java
+++ game/init/Launcher.java
@@ -1,9 +1,28 @@
 package game.init;
 
+import game_engine.frontend.graphics.Graphics;
+
 public class Launcher {
 
 	public static void main(String[] args) {
 
+		
+		
+	}
+	
+	// --- TEST CASES --- //
+	
+	/** Used to test the Game Engine
+	 * 
+	 * @return boolean[x] true if successful, otherwise failed.
+	 */
+	public boolean[] tests() {
+		
+		boolean[] returnThis = new boolean[1];
+		
+		
+		
+		return returnThis;
 	}
 
 }
---
+++ game_engine/frontend/graphics/Graphics.java
@@ -0,0 +1,45 @@
+package game_engine.frontend.graphics;
+
+public class Graphics {
+	
+	// -- Enum -- //
+	public enum graphicTypes {
+		UNKNOWN,
+		GUI
+	}
+	
+	// -- Object Classes -- //
+	
+	/**
+	 * Object that can be represented in various forms so it can be "painted" onto the screen
+	 *
+	 */
+	protected class graphicalObject {
+		
+		public graphicalObject() {
+			
+		}
+		
+	}
+	
+	// -- Graphics Classes -- //
+	
+	/**
+	 * graphicalUserInterface class contains all the graphical functions IN RELATION TO GUIs only.
+	 *
+	 */
+	public class graphicalUserInterface {
+		
+	}
+	
+	/**
+	 * Painter class contains all the graphical functions, excluding GUI functions, in which they draw stuff onto the screen.
+	 *
+	 */
+	public class painter {
+		
+		
+		
+	}
+	
+}

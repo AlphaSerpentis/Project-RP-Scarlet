d5043f821e0f00ccb90b9aab75355befceb5d5ef
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-02-14 10:38:04 -0600

    Removed Priority Value-related methods from IGraphics

--- game_engine/frontend/graphics/IGraphics.java
+++ game_engine/frontend/graphics/IGraphics.java
@@ -1,10 +1,8 @@
 package game_engine.frontend.graphics;
 
 public interface IGraphics {
-	public void setPriorityValue(int v);
 	public void setVisible(boolean v);
 	public void setPainted(boolean v);
-	public int getPriorityValue();
 	public boolean getVisible();
 	public boolean getPainted();
 }

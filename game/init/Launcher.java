cdd0c708a5d56f58caaab2bbe6b1758644a8434f
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2020-02-15 20:51:39 -0600

    Added Graphics test case

--- game/init/Launcher.java
+++ game/init/Launcher.java
@@ -1,5 +1,10 @@
 package game.init;
 
+import java.util.ArrayList;
+
+import game_engine.frontend.graphics.Graphics.*;
+import game_engine.frontend.graphics.Graphics.GraphicalObject;
+import game_engine.frontend.graphics.Graphics.Painter;
 import game_engine.frontend.graphics.main.Initializer;
 
 public class Launcher {
@@ -8,6 +13,8 @@ public class Launcher {
 		
 		System.out.println("[(game.init) Launcher.java] Starting Graphics Process");
 		new Initializer().start(1000,1000); // Start Graphics Process
+		
+		//new Launcher().tests();
 		
 	}
 	
@@ -21,6 +28,24 @@ public class Launcher {
 		
 		boolean[] returnThis = new boolean[1];
 		
+		// Start Graphics Test (Collections Sort)
+		System.out.println("Starting Graphics Test...");
+		
+		Painter p = new Painter(); // Create new Painter instance
+		ArrayList<GraphicalObject> g_obj = new ArrayList<GraphicalObject>();
+		
+		for(int i = 0; i < 100; i++) {
+			GraphicalObject randomTest = new GraphicalObject();
+			
+			randomTest.setPriorityValue((int) (Math.random() * 100));
+			
+			g_obj.add(randomTest);
+		}
+		
+		p.add(g_obj);
+		p.sortPriority();
+		
+		System.out.println("Graphics Test Result: " + p.getObjectsToBePainted());
 		
 		
 		return returnThis;
